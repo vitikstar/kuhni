2926226fc063d25ec81ef3ee5b0b4a22be72790e
Баранов Виктор <vitya.ak22071990@gmail.com>
2022-07-06 15:44:06 +0300

    блок 3d

---
+++ admin/language/ru-ru/extension/module/capacity.php
@@ -0,0 +1,16 @@
+<?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading
+$_['heading_title']    = '<img src="/image/cop.png" width=45>Получите бесплатный 3д проект';
+
+$_['text_module']      = 'Модули';
+$_['text_success']     = 'Настройки успешно изменены!';
+$_['text_edit']        = 'Редактирование';
+
+// Entry
+$_['entry_status']     = 'Статус';
+
+// Error
+$_['error_permission'] = 'У вас недостаточно прав для внесения изменений!';
---
+++ catalog/controller/extension/module/capacity.php
@@ -0,0 +1,6 @@
+<?php
+class ControllerExtensionModuleCapacity extends Controller {
+	public function index() {
+		return $this->load->view('extension/module/capacity', []);
+	} 
+} 
